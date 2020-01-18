ccd61075edd810e851927146ed96f5163c963b94
Ryan Kopinsky <hack@ryankopinsky.com>
2020-01-18 13:51:12 -0500

    Created new iOS project for VSiBL prototype

---
+++ VSiBL Prototype/VSiBL Prototype/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VSiBL Prototype
+//
+//  Created by User239 on 1/18/20.
+//  Copyright © 2020 VSiBL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
