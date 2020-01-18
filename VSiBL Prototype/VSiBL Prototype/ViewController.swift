854ff8c25575a1f84d2452dec6513f5355bffc3a
Ryan Kopinsky <hack@ryankopinsky.com>
2020-01-18 14:20:54 -0500

    Created new project based on RealityKit template

---
+++ VSiBL Prototype/VSiBL Prototype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VSiBL Prototype
+//
+//  Created by User239 on 1/18/20.
+//  Copyright © 2020 VSiBL. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
